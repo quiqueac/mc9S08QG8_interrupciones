c61196eb8f2eba5c2c7f39a482bd95881c159d8d
quiqueac <82471440+quiqueac@users.noreply.github.com>
2021-11-12 00:08:50 -0600

    Terminando...

--- Sources/main.asm
+++ Sources/main.asm
@@ -19,8 +19,8 @@
 ;
 ; variable/data section
 ;
-            ORG    RAMStart         ; Insert your data definition here
-ExampleVar: DS.B   1
+            ORG    $60         		; Insert your data definition here
+M60: DS.B   1
 
 ;
 ; code section
@@ -29,16 +29,46 @@ ExampleVar: DS.B   1
             
 
 _Startup:
-            LDHX   #RAMEnd+1        ; initialize the stack pointer
+			LDA	  	#$12			; WATCHDOG	
+            STA	  	SOPT1			; WATCHDOG
+            
+            LDHX   	#RAMEnd+1		; initialize the stack pointer
             TXS
-            CLI			; enable interrupts
-
-mainLoop:
-            ; Insert your code here
-            NOP
-
-            feed_watchdog
-            BRA    mainLoop
+            
+            LDA	  #$53    			; Configura IRQMOD=1, IRQPE=1, IRQPDD=1, IRQIE=1
+        	STA   IRQSC
+            
+            CLI						; enable interrupts
+            
+Inicio:
+			LDA		#$FF			; Mover al acumulador el valor inmediato FF hexadecimal
+			STA		PTBDD			; Todos los pines del puerto B son salidas
+			
+			MOV		#$00,M60			; Si es 00, la rotacion es hacia la izquierda
+									; Si es FF, la rotacion es hacia la derecha
+			
+			SEC						; Bit de carry encendido
+Rotar:	
+			LDA		#$FF			; Inicio del retardo
+			LDX		#$FF
+Retardo:
+			DBNZA	Retardo			; Decrementa el acumulador y brinca a la etiqueta "Retardo" si no es cero
+									; Si el acumulador es cero, realizará la instrucción "DBNZX	Retardo"
+									
+			DBNZX	Retardo			; Decrementa el registro X y brinca a la etiqueta "Retardo" si no es cero
+									; Si el registro X es cero, realizará la instrucción "ROL PTBD"
+			
+			LDA		M60				; Cargar en el acumulador el valor de la direccion de memoria 60 hexadecimal
+			BEQ 	Izquierda		; Si el acumulador es 0, se brinca a la etiqueta "Izquierda"
+			BRA		Derecha			; Si el acumulador no es 0, se brinca a la etiqueta "Derecha"
+Izquierda:	
+			ROL		PTBD			; Rotar a la izquierda el contenido del puerto B con el carry
+			BRA		Rotar			; Brincar a la etiqueta rotar
+Derecha:
+			ROR		PTBD			; Rotar a la derecha el contenido del puerto B con el carry
+			BRA		Rotar			; Brincar a la etiqueta rotar
+			
+			BRA		Rotar			; Brincar a la etiqueta rotar indefinidamente
 			
 ;**************************************************************
 ;* spurious - Spurious Interrupt Service Routine.             *
@@ -50,11 +80,27 @@ spurious:				; placed here so that security value
 			RTI
 
 ;**************************************************************
+;* SubrutinaDireccion - Usando PIN PTA5 como IRQ              *
+;*                                                            *
+;**************************************************************
+
+SubrutinaDireccion:	
+				SEI  						; Quita las IRQ
+				
+				BSET IRQSC_IRQACK,IRQSC		; Apagar la bandera IRQF
+				
+				CLC							
+				COM M60						; Operacion logica NOT con el valor de la direccion de memoria 60 hexadecimal
+				
+				CLI   						; Activa las IRQ
+				RTI   						; Termina la IRQ
+
+;**************************************************************
 ;*                 Interrupt Vectors                          *
 ;**************************************************************
 
             ORG	$FFFA
-
-			DC.W  spurious			;
-			DC.W  spurious			; SWI
-			DC.W  _Startup			; Reset
+			DC.W  SubrutinaDireccion   		; Para el pin IRQ 
+			
+			ORG	$FFFE
+			DC.W  _Startup					; Reset
